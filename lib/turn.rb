b6159f233d245e9fe35d9757a59b8cce7849e06e
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 02:33:09 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,4 @@
-board = {"   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "}
+puts board = {"   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "}
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
