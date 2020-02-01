6d97f51e877e4c5b5f43892cd4700120cd3df29f
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 02:58:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,6 +36,8 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     puts "valid move"
+    move(board, index)
+    display(board)
   else
     puts "try again"
   end
