9025f095265989093559346c6d87df357733e116
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 02:51:59 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,5 @@ def move(board, index, char="X")
 end
 
 def turn(board)
-  puts "Please enter 1-9"
-  user_input = gets.strip
-  index = input_to_index(user_input)
+  puts "Please enter 1-9:"
 end
